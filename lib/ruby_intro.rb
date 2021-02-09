48f8e9c988f5acd9e2e54711f23898f3e1d1f80e
EC2 Default User <ec2-user@ip-172-31-21-20.us-east-2.compute.internal>
2021-02-09 11:15:10 +0000

    assignment1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,63 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return arr.sum()
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  else
+    new_arr = arr.sort {|a,b| b <=> a}
+    return new_arr[0] + new_arr[1]
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.length < 2
+    return false
+  else
+    for i in 0..arr.length-1
+      for j in i+1..arr.length-1
+        if (arr[i] + arr[j]) == n
+          return true
+        end
+      end
+    end 
+    return false
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return s =~ /\A[b-df-hj-np-tv-z]/i 
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return s =~ /\A([01]*00|0)\Z/
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    if isbn == '' or price <= 0
+      raise(ArgumentError)
+    else
+      @isbn = isbn
+      @price = price
+    end  
+  end
+  attr_accessor :isbn
+  attr_accessor :price
+  def price_as_string()
+    return "$"+"%.2f"%@price
+  end
 end
